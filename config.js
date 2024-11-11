ee601afb81300853f97e0f9ea85a2867d903b209
Shaka56355 <shakadon416@gmail.com>
2024-11-12 01:40:45 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "b752gS6S#T-HfNZG3zZ6MD72PVuOxkpDu0TdLACcTkkYVvUbm2H0",
+SESSION_ID: process.env.SESSION_ID || "3roTjBDa#dpGB8Nq73n4DX5u5fJAUdQS3McFcPRmVk80ST_bPTD8",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
@@ -14,6 +14,7 @@ AUTO_REPLY: process.env.AUTO_REPLY || "false",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/2a06381b260c3f096a612.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M SILENT-SOBX-MD WHATSAPP BOT 😊♻️",
 ANTI_LINK: process.env.ANTI_LINK || "true",
+AUTO_ADMIN: process .env.AUTO_ADMIN || "true",   
 ANTI_BAD: process.env.ANTI_BAD || "true",
 PREFIX: process.env.PREFIX || ".",
 FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
